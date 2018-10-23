25e0795cc99a1299ae87248804a80d8a9adfb7cb
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2018-10-23 21:33:55 +0200

    cleaner components and service setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CleanerContainer from '../containers/CleanerContainer';
 import '../assets/css/App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                 </header>
+                <CleanerContainer />
             </div>
         );
     }
