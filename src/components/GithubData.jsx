7467056ba78ad8e6181740d154f05dd3dda8a617
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2018-10-28 09:02:54 +0100

    table data and component

--- src/components/GithubData.jsx
+++ src/components/GithubData.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 
+import Table from '../components/Table';
+
+
 class GithubData extends Component {
+
+    dataToDisplay = [
+        {
+            key: 'full_name',
+            label: 'Name'
+        },
+        {
+            key: 'description',
+            label: 'Description'
+        }
+    ];
+
 
     render() {
         if (this.props.data.length === 0) {
             return null;
         }
 
-        return this.props.data.map((el) => {
-            return <div key={el.id}>{el.name}</div>;
-        });
+        return (
+            <Table 
+                data={this.props.data} 
+                columns={this.dataToDisplay} 
+            />
+        );
     }
 }
 
---
+++ src/components/Table.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+
+    renderRows = () => {
+        return this.props.data.map((el) => {
+            return (
+                <tr key={el.id}>
+                    {this.renderCells(el)}
+                </tr>
+            );
+        });
+    }
+
+
+    renderCells = (row) => {
+        return this.props.columns.map((el) => {
+            return <td key={el.key}>{row[el.key]}</td>
+        });
+    }
+
+
+    renderTableHeaders = () => {
+        return this.props.columns.map((el) => {
+            return <th key={el.key}>{el.label}</th>
+        });
+    }
+
+
+    render() {
+        if (this.props.data.length === 0) {
+            return null;
+        }
+
+        return (
+            <table cellPadding="0" cellSpacing="0">
+                <tbody>
+                    <tr>
+                        {this.renderTableHeaders()}
+                    </tr>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default Table;
