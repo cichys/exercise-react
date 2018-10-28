ebf1dd953786e5582d473c38c782ff4f725b3119
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2018-10-28 12:21:12 +0100

    tests

--- src/tests/GithubContainer.test.js
+++ src/tests/GithubContainer.test.js
@@ -13,6 +13,7 @@ describe('Github Container component', () => {
 
     beforeEach(() => {
         mock.onGet('https://api.github.com/repositories?since=1').reply(200, response);
+        mock.onGet('https://api.github.com/repositories?since=370').reply(200, response);
     });
 
 
@@ -26,12 +27,43 @@ describe('Github Container component', () => {
     });
 
 
-    it('renders table', (done) => {
+    it('save data in state', (done) => {
         const wrapper = shallow(<GithubContainer />);
         setTimeout(o => {
             expect(wrapper.state('data').length).toEqual(100);
             done();
-        }, 100)
+        }, 100);
+    });
+
+
+    it('call github api with parameter "since" at 1', () => {
+        let spy = jest.spyOn(axios, "get");
+        shallow(<GithubContainer />);
+        expect(spy).toHaveBeenCalledWith('https://api.github.com/repositories?since=1', expect.anything());
+    });
+
+
+    it('call github api on next page with correct parameter "since"', (done) => {
+        let spy = jest.spyOn(axios, "get");
+        const wrapper = shallow(<GithubContainer />);
+        setTimeout(o => {
+            wrapper.instance().nextPage();
+            expect(spy).toHaveBeenCalledWith('https://api.github.com/repositories?since=370', expect.anything());
+            done();
+        }, 100);
+    });
+
+
+    it('call github api on previous page with correct parameter "since"', (done) => {
+        let spy = jest.spyOn(axios, "get");
+        const wrapper = shallow(<GithubContainer />);
+        setTimeout(o => {
+            wrapper.instance().nextPage();
+            expect(spy).toHaveBeenCalledWith('https://api.github.com/repositories?since=370', expect.anything());
+            wrapper.instance().previousPage();
+            expect(spy).toHaveBeenCalledWith('https://api.github.com/repositories?since=1', expect.anything());
+            done();
+        }, 100);
     });
 });
 
